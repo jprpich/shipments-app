3729de063f55e89863eff444c0522462c6f24357
Joshua <joshuaprpich@gmail.com>
2019-02-25 10:16:29 -0500

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@ Delivery.destroy_all
 
 random_latitude = 51.501564
 random_longitude = -0.141944
-10.times do 
+20.times do 
   Location.create(city: Faker::Address.city,  latitude: random_latitude,longitude: random_longitude)
   random_latitude += 0.05
   random_longitude -= 0.01
 end
 
-10.times do 
+20.times do 
   Vendor.create(name: Faker::Company.name)
 end
 
@@ -23,7 +23,7 @@ Vendor.pluck(:id).each do |vendor_id|
   end
 end
 
-30.times do 
+1000.times do 
   random_day = (rand * 27).to_i + 1
   random_date = Date.new(2019, 2, random_day)
   random_delivery_time = (rand * 10).to_i + 1
